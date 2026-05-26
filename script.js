b6d870e19e36b8cdeba63ec893ac2a2d01d7e430
khushiiii-error404 <khushibhure0503@gmail.com>
2026-05-26 22:19:37 +0530

    add random boxes button and event

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const btn =document.querySelector("button");
 const container = document.querySelector("#container");
+
   for(let i=1;i<256;i++){
       const div=document.createElement("div");
-      div.style.border ="1px solid white";
       div.style.width=`${700/16}px`;
       div.style.height=`${700/16}px`;
       div.style.boxSizing ='border-box';
       container.appendChild(div);
 }
+function randomcolor(){
+    return `hsl(${Math.random ()* 360}, 70%, 60%)`
+}
 
+btn.addEventListener('click',(e)=>{
+    container.textContent=""
+    let input=prompt("Enter number of grids (Limit : 100)")
+    const size = 700/input;
+    for(let i=1;i<input * input ;i++){
+      const div=document.createElement("div");
+      div.style.width=`${size}px`;
+      div.style.height=`${size}px`;
+      div.style.boxSizing ='border-box';
+      container.appendChild(div);
+}});
